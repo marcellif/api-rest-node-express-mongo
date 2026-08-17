2f68e97b3c93c6d60a1080714c8b41192abc68bf
Marcelli Pires Faria <marcelli.faria@onidata.com>
2026-08-17 20:33:49 -0300

    adding route Livros phase one

--- src/app.js
+++ src/app.js
@@ -2,9 +2,24 @@ import express from "express";
 
 const app = express();
 
+const livros =[
+    {
+        id: 1,
+        titulo: "O Senhor dos aneis"
+    },
+    {
+        id: 2,
+        titulo: "o Hobbit"
+    }
+]
+
 
 app.get("/", (req,res) =>{
     res.status(200).send("Curso de Node.js");
+})
+
+app.get("/livros", (req,res) =>{
+    res.status(200).json(livros);
 })
 
 export default app;
