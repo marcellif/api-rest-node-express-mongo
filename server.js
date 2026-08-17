a5f922516c5b23d66c2e5ab6e7bb09c505d8cea1
Marcelli Pires Faria <marcelli.faria@onidata.com>
2026-08-17 19:41:54 -0300

    Configura .gitignore e adiciona arquivos do projeto

---
+++ server.js
@@ -0,0 +1,18 @@
+import http from "http";
+
+const PORT = 3000;
+
+const rotas ={
+    "/": "Curso de express api",
+    "/livros": "Entrei na rota livros",
+    "/autores": "Entrei na rota autores"
+}
+
+const server = http.createServer((req, res) =>{
+    res.writeHead(200, {"content-type":"text/plain"});
+    res.end(rotas[req.url])
+})
+
+server.listen(PORT, () =>{
+    console.log("servidor escutando!")
+})
